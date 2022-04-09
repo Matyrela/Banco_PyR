93bc962c86b8b85ef7f83fc0f17a86fc6e91da56
Matías Varela <mativaresc@gmail.com>
2022-04-08 21:51:02 -0300

    g

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 require_once 'config/config.php';
 require_once 'models/cssInjector.php';
 require_once 'models/navbar.php';
+require_once 'models/footer.php';
 ?>
---
+++ models/footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-light text-center text-white">
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Copyright: <a class="text-white" href="https://github.com/Matyrela">Matías Varela</a>
+  </div>
+</footer>
